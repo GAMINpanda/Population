091279d0fbb93d8b3a6091f898b8d0cfd812f6ee
Alexander Mitchiner <mitchinerfamily1@gmail.com>
2022-02-23 21:58:39 +0000

    scuffed rn

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Simulator
 {
     class Program
     { //Want to create some sort of program to model a population based on changes from the outside world, sort of joke based stuff
       //Based on historical variation and stuff once done a paragraph will explain the history the population
-
-        static void NewYear(int yearnum)
+        static List<double> Run(double population, double difficulty, int duration) //function to run the program
         {
-            //Going to be the function called every year
+            double percent = (0.001 / difficulty);
+            double birth = 1 + percent; 
+            double death = 1 - percent;
+
+            double innovation = 1;
+            double invrate = 1 + percent;
+
+            double food = 0.98;
+
+            for (int i = 0; i <= duration; i++) //runs iterations
+            {
+                population = ((population + (population * 1.05)) * birth) - ((population * 0.95) * death);
+                innovation = innovation * invrate + (innovation * (1 - food));
+
+                if (population > (innovation * 1000000)) //innovation can only support so much population
+                {
+                    population = population * 0.98;
+                }
+
+                birth = (food * birth) + (innovation / 1000);
+                death = (death / food) - (innovation / 1000);
+
+                if (population < 100)
+                {
+                    break;
+                }
+            }
+
+            return new List<double>() { population, innovation};
         }
         static void Main(string[] args)
         {
@@ -17,13 +46,16 @@ namespace Simulator
             Console.WriteLine("===========================================");
 
             Console.WriteLine("Give=starting=population=(1000-100,000)==== ");
-            int pop = Convert.ToInt32(Console.ReadLine());
+            double population = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Give=Difficulty=(0-1)====================== ");
-            double diff = Convert.ToDouble(Console.ReadLine());
+            double difficulty = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Give=Duration=(100-5,000)================== ");
-            int dur = Convert.ToInt32(Console.ReadLine());
+            int duration = Convert.ToInt32(Console.ReadLine());
+
+            List<double> output = Run(population,difficulty,duration);
+            Console.WriteLine("Final=Population:={0}=Final=Innovation:={1}",output[0], output[1]);
         }
     }
 }
